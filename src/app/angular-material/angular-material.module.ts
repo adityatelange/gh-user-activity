227a5454d708265d72cb9680fe3ee03cfa7f7151
Aditya Telange <21258296+adityatelange@users.noreply.github.com>
2020-07-27 18:53:54 +0530

    add toolbar and simple input box

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatCardModule,
+    MatInputModule,
+    MatIconModule
   ]
 })
 export class AngularMaterialModule { }
