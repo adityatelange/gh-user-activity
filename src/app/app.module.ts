e338f38dfdca03bccf6ef4190805c0b068dda514
Aditya Telange <21258296+adityatelange@users.noreply.github.com>
2020-07-31 13:07:26 +0530

    amg-mat move

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AngularMaterialModule } from './angular-material/angular-material.module';
+import { AngularMaterialModule } from './angular-material.module';
 import { HomeComponent } from './components/home/home.component';
 import { MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
